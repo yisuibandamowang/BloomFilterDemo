280cba7557d64b4542457f40ab732bfc35f0fa8a
我只有一岁半 <2307585872@qq.com>
2025-10-22 14:38:48 +0800

    项目初始化

---
+++ src/main/java/com/xiaohei/bloomfilterdemo/BloomFilterDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xiaohei.bloomfilterdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloomFilterDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloomFilterDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xiaohei/bloomfilterdemo/BloomFilterDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaohei.bloomfilterdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloomFilterDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
